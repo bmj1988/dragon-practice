f9e52db0d6357ba15d01233ff01b7009fca60da2
Kristopher Snow <corvus.snow87@gmail.com>
2023-05-18 18:59:43 -0400

    friendly dragon passing

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,18 +1,19 @@
 class Dragon {
-  constructor(name, color){
+  constructor(name, color) {
     this.name = name;
     this.color = color;
   }
-  breathesFire(){
-    return (`${this.name} breathes fire everywhere! BURN!!!!`)
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
   }
-  static getDragons(...dragons){
+  static getDragons(...dragons) {
     let arr = [];
-    dragons.forEach(el => arr.push(el.name))
+    dragons.forEach((el) => arr.push(el.name));
     return arr;
   }
 }
 
+module.exports = Dragon;
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
