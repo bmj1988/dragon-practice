a674587b2bb8397cb70a2194020c846740f21b34
Kristopher Snow <corvus.snow87@gmail.com>
2023-05-18 18:48:03 -0400

    dragon passing

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,17 @@
-// Your code here
+class Dragon {
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+  }
+  breathesFire(){
+    return (`${this.name} breathes fire everywhere! BURN!!!!`)
+  }
+  static getDragons(...dragons){
+    let arr = [];
+    dragons.forEach(el => arr.push(el.name))
+    return arr;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
